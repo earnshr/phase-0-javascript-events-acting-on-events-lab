fa7f0a16b9df87ec4ef9f47b8b00bb35b54d41b3
bexearn12 <earnshr@gmail.com>
2021-06-02 13:41:12 -0500

    commit tree b9c281fdc986c534203e53eec16caef37b91fca6

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 const dodger = document.getElementById("dodger");
 
+dodger.style.backgroundColor = ("#FF69B4")
+dodger.style.bottom = "0px";
+dodger.style.left = "0px"
+
+document.addEventListener("keydown", function(event) {
+    console.log(event);
+})
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+    }
+})
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-
-    if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left -1}px`;
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
     }
 }
 
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-
-    if (left < 360) {
-        dodger.style.left = `${left +1}px`;
+    dodger.style.left = `${left -1}px`;
+        if (left < 360) {
+            dodger.style.left = `${left +1}px`;
     }
 }
 
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
         moveDodgerLeft();
     }
-    else if (e.key === "ArrowRight") {
-        moveDodherRight();
+    else if (event.key === "ArrowRight") {
+        moveDodgerRight();
     }
 });
+
